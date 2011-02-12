c33be7ad3e54a4a73dec1f6deb7ba249c178e61b
Mark Van Holstyn <mvanholstyn@gmail.com>
2011-02-11 21:14:19 -0500

    more efficient query

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
   end
   
   def next
-    task = Task.all.not_in(:status => ["pulled", "has_result"]).first
+    task = Task.not_in(:status => ["pulled", "has_result"]).first
     if task
       task.update_attributes(:status => "pulled")
       data = { :id => task.id, :fn => task.job.function, :data => task.data }
