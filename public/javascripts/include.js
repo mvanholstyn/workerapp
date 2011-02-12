4bca247432da5fb4ccb5456c2b67c0b4301757d3
Mark Van Holstyn <mvanholstyn@gmail.com>
2011-02-11 21:56:45 -0500

    remove alerts

--- public/javascripts/include.js
+++
@@ -1,7 +0,0 @@
-alert("working...");
-var worker = new Worker("/javascripts/worker.js");
-alert(worker);
-worker.onmessage = function(e) {
-  alert(e.data);
-};
-worker.postMessage(["workerapp.heroku.com", "API_KEY"]);
